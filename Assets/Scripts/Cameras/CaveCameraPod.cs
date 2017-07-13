2dc58b19b2ceb1d7c8360c2cbcce877583c3e7cb
Tana Tanoi <tana.tanoi@gmail.com>
2017-07-13 15:16:22 +1200

    Change sensitivity of Wheel

--- Assets/Scripts/Cameras/CaveCameraPod.cs
+++ Assets/Scripts/Cameras/CaveCameraPod.cs
@@ -9,12 +9,14 @@ public class CaveCameraPod : MonoBehaviour {
     public GameObject rightCamera;
 
     public void SetAllCameras() {
+        EnableAllDisplays();
         frontCamera.SetActive(true);
         leftCamera.SetActive(true);
         rightCamera.SetActive(true);
     }
 
     public void SetFrontCamera() {
+        EnableAllDisplays();
         frontCamera.SetActive(true);
         leftCamera.SetActive(false);
         rightCamera.SetActive(false);
@@ -24,6 +26,17 @@ public class CaveCameraPod : MonoBehaviour {
         frontCamera.SetActive(false);
         leftCamera.SetActive(false);
         rightCamera.SetActive(false);
+    }
+
+    private void EnableAllDisplays() {
+        Display[] displays = Display.displays;
+
+        if (displays.Length > 1)
+        {
+            displays[1].Activate(1920, 1080, 60);
+            displays[2].Activate(1920, 1080, 60);
+            displays[3].Activate(1920, 1080, 60);
+        }
     }
 }
 
